92d295319d61e32155fc3daba52af18159253c84
Vaibhav Makwana <121240124+Vaibhav31mak@users.noreply.github.com>
2025-07-05 15:04:49 +0530

    STEP 3 - RED - Failing test for newline as valid delimiter

--- StringCalculator.Tests/CalculatorTests.cs
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -39,6 +39,13 @@ public class CalculatorTests
         var result = calc.Add("1,2,3,4");
         Assert.Equal(10, result);
     }
+    [Fact]
+    public void Add_NumbersWithNewLinesAndCommas_ReturnsSum()
+    {
+        var calc = new Calculator();
+        var result = calc.Add("1\n2,3");
+        Assert.Equal(6, result);
+    }
 
 
 }
