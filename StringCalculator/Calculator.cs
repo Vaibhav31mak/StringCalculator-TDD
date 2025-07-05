5be9d4831e1ca2e6a36633b8f67e25075ee43a84
Vaibhav Makwana <121240124+Vaibhav31mak@users.noreply.github.com>
2025-07-05 14:57:06 +0530

    STEP 1 - GREEN - Handle two comma-separated numbers and return sum

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -7,6 +7,18 @@ public class Calculator
         if (string.IsNullOrEmpty(numbers))
             return 0;
 
+        if (numbers.Contains(","))
+        {
+            var parts = numbers.Split(',');
+            int sum = 0;
+            foreach (var part in parts)
+            {
+                sum += int.Parse(part);
+            }
+            return sum;
+        }
+
         return int.Parse(numbers);
     }
+
 }
